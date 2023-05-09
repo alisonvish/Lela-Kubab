53741f0e9f49a8432a5a72b66ef8b82805f5e2d0
alisonvish <118972461+alisonvish@users.noreply.github.com>
2023-05-09 19:32:36 +0300

    Update alg_7.py

--- alg_7.py
+++ alg_7.py
@@ -15,23 +15,47 @@ lst = [3, 14, 1, 7, 9, 8, 11, 6, 4, 2]
 # Решение
 
 
-lst = sorted((3, 14, 1, 7, 9, 8, 11, 6, 4, 2))
- 
-n = len(lst) 
-print ("Sorted array is")
-for i in range(n):
-    print ("%d" %(lst[i]))
+def heapify(numbers, n, i):
+    largest = i    # Initialize largest as root. (Инициализировать наибольший как root)
+    l = 2 * i + 1   # left = 2*i + 1
+    r = 2 * i + 2   # right = 2*i + 2
 
-# Ответ
 
-Sorted array is
-1
-2
-3
-4
-6
-7
-8
-9
-11
-14
+ # Проверяем существует ли левый дочерний элемент больший, чем корень
+
+    if l <= n and numbers[l] > numbers[largest]:
+        largest = l
+
+    # Проверяем существует ли правый дочерний элемент больший, чем корень
+
+    if r <= n and numbers[largest] > numbers[r]:
+        largest = r
+
+    # Заменяем корень, если нужно
+    if largest == i:
+          return
+    else:
+        numbers[largest], numbers[i] = numbers[i], numbers[largest]
+        heapify(numbers , n, largest)
+
+
+
+# Основная функция для сортировки 
+def build_max_heap(numbers):
+    middle = len(numbers) // 2
+    for idx in reversed(range(0, middle + 1)):
+	    heapify(numbers, idx, len(numbers) - 1)
+
+def heap_sort(numbers):
+    build_max_heap(numbers)
+    for idx in reversed(range(0, len(numbers))):
+        numbers[0], numbers[idx] = numbers[idx], numbers[0]
+        heapify(numbers, 0, idx - 1)
+numbers = [3, 14, 1, 7, 9, 8, 11, 6, 4, 2]
+heap_sort(numbers)
+print(numbers)
+
+
+#ответ
+
+[14, 1, 7, 9, 8, 11, 6, 4, 2, 3]
