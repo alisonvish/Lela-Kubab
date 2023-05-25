641f505296c3f7759ba826574d567c740335458f
Kristina Romanovskaya <124786607+kr27904@users.noreply.github.com>
2023-05-25 17:27:46 +0300

    Update 15_x_and_o.py

--- 15_x_and_o.py
+++ 15_x_and_o.py
@@ -9,3 +9,23 @@
 #   строка "zpzpzpp" => true # когда нет "x" и "o", должно быть возвращено значение true
 #   строка "zzoo" => false
 
+
+
+
+# решение
+
+str = input()
+count_x = 0
+count_o = 0
+for letter in str:
+    if letter == 'x':
+        count_x +=1
+    elif letter == 'o':
+        count_o +=1
+if count_x == count_o:
+    print(True)
+else:
+    print(False)
+    
+    
+ 
